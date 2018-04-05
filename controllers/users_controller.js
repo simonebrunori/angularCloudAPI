176fe2bf7169effd4223ff084a8729ea5af431d1
Simone Brunori <brunorisimone@gmail.com>
2018-04-05 18:10:40 +0100

    Aggiunta route login

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -88,12 +88,12 @@ module.exports = (router) => {
                                                 } else {
                                                     // Check if validation error is in the type field
                                                     if (err.errors.type) {
-                                                        res.json({
+                                                        res.status(500).json({
                                                             success: false,
                                                             message: err.errors.type.message
                                                         }); // Return error
                                                     } else {
-                                                        res.json({
+                                                        res.status(500).json({
                                                             success: false,
                                                             message: err
                                                         }); // Return any other error not already covered
@@ -110,7 +110,7 @@ module.exports = (router) => {
                                     }
                                 }
                             } else {
-                                res.json({
+                                res.status(200).json({
                                     success: true,
                                     message: 'Acount registered!'
                                 }); // Return success
@@ -126,6 +126,70 @@ module.exports = (router) => {
     /* ========
   LOGIN ROUTE
   ======== */
+
+  router.post('/login', (req, res) => {
+    // Check if username was provided
+    if (!req.body.username) {
+      res.json({ success: false, message: 'No username was provided' }); // Return error
+    } else {
+      // Check if password was provided
+      if (!req.body.password) {
+        res.json({ success: false, message: 'No password was provided.' }); // Return error
+      } else {
+        // Check if username exists in database
+        User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+          // Check if error was found
+          if (err) {
+            res.json({ success: false, message: err }); // Return error
+          } else {
+            // Check if username was found
+            if (!user) {
+              res.json({ success: false, message: 'Username or password not valid' }); // Return error
+            } else {
+              const validPassword = user.comparePassword(req.body.password); // Compare password provided to password in database
+              // Check if password is a match
+              if (!validPassword) {
+                res.json({ success: false, message: 'Username or password not valid' }); // Return error
+              } else {
+                const token = jwt.sign({ userId: user._id }, db.secret, { expiresIn: '24h' }); // Create a token for client
+                res.json({
+                  success: true,
+                  message: 'Success!',
+                  token: token,
+                  user: {
+                    username: user.username
+                  }
+                }); // Return success and token to frontend
+              }
+            }
+          }
+        });
+      }
+    }
+  });
+
+   /* ================================================
+  MIDDLEWARE - Used to grab user's token from headers
+  ================================================ */
+  router.use((req, res, next) => {
+    const token = req.headers['authorization']; // Create token found in headers
+    // Check if token was found in headers
+    if (!token) {
+      res.status(406).json({ success: false, message: 'No token provided' }); // Return error
+    } else {
+      // Verify the token is valid
+      jwt.verify(token, db.secret, (err, decoded) => {
+        // Check if error is expired or invalid
+        if (err) {
+          res.status(500).json({ success: false, message: 'Token invalid: ' + err }); // Return error for token validation
+        } else {
+          req.decoded = decoded; // Create global variable to use in any request beyond
+          next(); // Exit middleware
+        }
+      });
+    }
+  });
+
 
 
 
--- index.js
+++ index.js
@@ -29,13 +29,13 @@ mongoose.connect(config.uri, {
 app.use(cors({ origin: 'http://localhost:4200' })); // Allows cross origin in development only
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
+app.use(express.static("C:/Users/Simone/Desktop/frontend")); // Provide static directory for frontend      __dirname + '/public'
 app.use('/users', users); // Use Users Controller in application
 
 // Connect server to Angular 2 Index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/public/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join("C:/Users/Simone/Desktop/frontend/dist/index.html"));    //__dirname + '/public/index.html'
+});
 
 // Start Server: Listen on port 8080
 app.listen(port, () => {
