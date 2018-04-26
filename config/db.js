c1514570c656f1adfb09c02526b7c7208ed90abd
Simone Brunori <brunorisimone@gmail.com>
2018-04-26 18:47:09 +0100

    modifiche config/db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 
 // Export config object
 module.exports = {
-  uri: "mongodb://localhost:27017/ncloudlocal", // Databse URI and database name  mongodb://admin:admin@ds129352.mlab.com:29352/ngcloud           
+  // Databse URI and database name  mongodb://admin:admin@ds129352.mlab.com:29352/ngcloud
+  // uri: "mongodb://localhost:27017/ncloudlocal",            
+  uri: "mongodb://admin:admin@ds129352.mlab.com:29352/ngcloud",
   secret: crypto, // Cryto-created secret
   db: "ngcloud", // Database name
 }
